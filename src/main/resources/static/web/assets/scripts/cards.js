95db586de533ddfe1a0e79075cb12ef73664d9ff
Bruno Ferreira <fbrunomarcos@gmail.com>
2023-08-18 01:33:31 -0300

    cambio header

--- src/main/resources/static/web/assets/scripts/cards.js
+++ src/main/resources/static/web/assets/scripts/cards.js
@@ -24,11 +24,14 @@ createApp({
                     this.cards = this.clients.cards
                     this.debit= this.cards.filter(card=> card.type == "DEBIT")
                     this.credit= this.cards.filter(card=> card.type == "CREDIT")
+
                     this.fromDateDebit = this.debit.map(date => date.fromDate.slice(2,7))
                     this.thruDateDebit = this.debit.map(date => date.thruDate.slice(2,7))
+                    
                     this.fromDateCredit = this.credit.map(date => date.fromDate.slice(2,7))
                     this.thruDateCredit= this.credit.map(date => date.thruDate.slice(2,7))
-                localStorage.setItem('client',JSON.stringify(this.clients)) 
+                
+                    localStorage.setItem('client',JSON.stringify(this.clients)) 
                 })
         }
     }
